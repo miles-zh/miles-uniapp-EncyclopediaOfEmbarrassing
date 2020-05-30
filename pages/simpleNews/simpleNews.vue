241f7556d3d97ce9b75899ae5290e946162ce236
miles-zh <milesmatheson@sina.com>
2020-05-30 22:12:46 +0800

    组件修改了

--- pages/simpleNews/simpleNews.vue
+++ pages/simpleNews/simpleNews.vue
@@ -1,87 +1,23 @@
 <template>
 	<view>
-		<!-- 顶部导航 -->
-		<uniNavBar fixed background-color="#2196f3" class="top-tab" @clickRight="goPublish" @clickLeft="goBack">
-			<text class="fa fa-chevron-left" slot="left"></text>
-			<view class="top-tab-title">
-				<view :class="['title',titleTab === 'follow'?'title-active':'']" @tap="changeTitleTab('follow')">
-						关注
-					<view class="line"></view>
-				</view>
-				<view :class="['title',titleTab === 'topic'?'title-active':'']" @tap="changeTitleTab('topic')">
-						话题
-					<view class="line"></view>
-				</view>
-			</view>
-			<text slot='right' class="fa fa-pencil"></text>
-		</uniNavBar>
+		ddd
 	</view>
 </template>
 
 <script>
-	import uniNavBar from '@/components/uni-nav-bar/uni-nav-bar.vue'
 	export default {
 		data() {
 			return {
-				titleTab:'follow'
+				
 			}
 		},
 		methods: {
-			changeTitleTab(tab){
-				this.titleTab=tab
-			},
-			goBack(){
-				uni.navigateBack({
-					delta:1
-				})
-			},
-			goPublish(){
-				uni.navigateTo({
-					url:'/pages/publish/publish'
-				})
-			}
-		},
-		components:{
-			uniNavBar
+			
 		}
 	}
 </script>
 
-<style scoped lang="scss">
-	.top-tab{
-		.fa-chevron-left,.fa-pencil{
-			color: #fff;
-		}
-		.top-tab-title{
-			display: flex;
-			align-items: center;
-			justify-content: space-evenly;
-			width: 100%;
-			color: #555;
-			height: 50rpx;
-			.title{
-				// padding: 10rpx;
-				display: flex;
-				align-items: center;
-				flex-direction: column;
-				justify-content: center;
-				height: 50rpx !important; 
-				position: relative;
-				.line{
-					height: 5rpx;
-					
-					width: 70rpx;
-					position: absolute;
-					bottom: -10rpx;
-				}
-			}
-			.title-active{
-				
-				color: #fff;
-				.line{
-					background-color: #fff;
-				}
-			}
-		}
-	}
+<style>
+
 </style>
+
